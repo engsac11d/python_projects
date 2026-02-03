42c7b2f4498d555ce21e304b259f3538b9995b58
saeed <engsaciidessa@gmail.com>
2026-02-03 13:34:36 +0300

    create a madlibs game

---
+++ program1.py
@@ -0,0 +1,9 @@
+# MADBLIS GAME
+city= input("enter a city like (muqdisho , baraawe, marko): ")
+adjective1 = input("enter an adjeticy like (messy ,beuty ,dirty): ")
+adjective2= input("enter a word that end ing : ")
+noun=input ("enter your best freind name : ")
+print(f"today i go to {city} it was beutifull")
+print(F"it was {adjective1}")
+print(F"just we go for {adjective2}")
+print(F"i was with  my friend {noun}")
